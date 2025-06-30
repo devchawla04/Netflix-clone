631a61c803d847bbf687fd2835002d8da4db674e
Dev Chawla <devchawla.04@gmail.com>
2025-06-30 14:39:56 +0530

    minor fixes

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,28 +1,33 @@
-import React, { useEffect, useState } from 'react'
-import './navbar.css'
+import React, { useEffect, useState } from "react";
+import "./navbar.css";
 function Navbar() {
-    const [show,handlShow] = useState(false);
-function Sfunc(){
-        if(window.scrollY > 100){
-            handlShow(true);
-        }
-        else handlShow(false);
-}
-useEffect(()=>{
-window.addEventListener("scroll",Sfunc);
-return () => {
-    window.removeEventListener('scroll', Sfunc);
-  };
-},[]);
-
+  const [show, handlShow] = useState(false);
+  function Sfunc() {
+    if (window.scrollY > 100) {
+      handlShow(true);
+    } else handlShow(false);
+  }
+  useEffect(() => {
+    window.addEventListener("scroll", Sfunc);
+    return () => {
+      window.removeEventListener("scroll", Sfunc);
+    };
+  }, []);
 
   return (
-  <div className={`nav ${show && "nav_black"}`}>
-    <img  className='nav__logo'
-    src="https://upload.wikimedia.org/wikipedia/commons/0/08/Netflix_2015_logo.svg" alt="Netflix Logo" />
-  <img className='nav__avatar' src="https://upload.wikimedia.org/wikipedia/commons/0/0b/Netflix-avatar.png" alt="Avatar Logo" />
-  </div>
-  )
+    <div className={`nav ${show && "nav-black"}`}>
+      <img
+        className="nav-logo"
+        src="https://upload.wikimedia.org/wikipedia/commons/0/08/Netflix_2015_logo.svg"
+        alt="Netflix Logo"
+      />
+      <img
+        className="nav-avatar"
+        src="https://upload.wikimedia.org/wikipedia/commons/0/0b/Netflix-avatar.png"
+        alt="Avatar Logo"
+      />
+    </div>
+  );
 }
 
-export default Navbar
+export default Navbar;
